1540ec3e1db1c69c63a591d1d74b4e8e934c6160
A10336DIRPa131 <A10336DIRPa131@accenture.com>
2019-06-27 21:07:26 -0300

    cofigurando o formulário

--- src/app/shelter/reservation/reservation.module.ts
+++ src/app/shelter/reservation/reservation.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ReservationComponent } from './reservation.component';
 import { RouterModule } from '@angular/router';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ReservationComponent],
   imports: [
     CommonModule,
     RouterModule,
-    NgbDropdownModule
+    NgbDropdownModule,
+    FormsModule
   ],
   exports: [
     CommonModule,
